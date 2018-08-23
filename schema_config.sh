9183bc7df09f01453ae0b9642ae9476ecb1a57a2
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2018-08-23 23:51:33 +0530

    filters working TODO:add vendor to corpus,synonyms

---
+++ schema_config.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"url",
+     "indexed":false,
+     "type":"string",
+     "stored":true }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"title",
+     "indexed":false,
+     "type":"string",
+     "stored":true }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"summary",
+     "indexed":false,
+     "type":"string",
+     "stored":true }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"specs",
+     "indexed":true,
+     "type":"text_general",
+     "stored":false }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"price",
+     "indexed":true,
+     "type":"pfloats",
+     "stored":true,
+     "docValues":true}
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"catogory",
+     "indexed":true,
+     "type":"text_general",
+     "stored":false }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"brand",
+     "indexed":true,
+     "type":"text_general",
+     "stored":false }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"model_id",
+     "indexed":true,
+     "type":"text_general",
+     "stored":false }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-field":{
+     "name":"all_data",
+     "indexed":true,
+     "type":"text_general",
+     "stored":true,
+     "multiValued":true }
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":{
+     "source":"title",
+     "dest":"all_data"}
+}' http://localhost:8983/solr/test/schema
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":{
+     "source":"summary",
+     "dest":"all_data"}
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":{
+     "source":"catogory",
+     "dest":"all_data"}
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":{
+     "source":"brand",
+     "dest":"all_data"}
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":{
+     "source":"model_id",
+     "dest":"all_data"}
+}' http://localhost:8983/solr/test/schema
+
+
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":{
+     "source":"specs",
+     "dest":"all_data"}
+}' http://localhost:8983/solr/test/schema
